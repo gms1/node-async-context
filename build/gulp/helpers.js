1d8e4b4167876baf1ef2170797b7cc32025c4ade
gms1 <www.gms@gmx.at>
2017-06-29 07:18:05 +0200

    chore(package): update typescript 2.4.1, @types/..

--- build/gulp/helpers.js
+++ build/gulp/helpers.js
@@ -5,7 +5,28 @@ const path = require('path');
 function globify(...paths) {
   return path.join(...paths, '**', '*');
 }
+
+
+function updatePackageDependenciesVersions(deps, pkgPrefix, pkgVersion) {
+  if (!deps) return;
+  Object.getOwnPropertyNames(deps)
+      .filter((name) => {return name.startsWith(`${pkgPrefix}/`)})
+      .forEach((name) => {deps[name] = pkgVersion});
+}
+
+function updatePackageVersions(data, name, version) {
+  data.version = version;
+
+  const pkgNames = name.split('/');
+  if (pkgNames.length>1) {
+    const pkgPrefix = pkgNames[0];
+    updatePackageDependenciesVersions(data.dependencies, pkgPrefix, version);
+    updatePackageDependenciesVersions(data.devDependencies, pkgPrefix, version);
+    updatePackageDependenciesVersions(data.peerDependencies, pkgPrefix, version);
+  }
+  return data;
+}
+
+
 module.exports.globify = globify;
-
-
-
+module.exports.updatePackageVersions = updatePackageVersions;
