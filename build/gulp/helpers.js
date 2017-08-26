dd6a384b43c22a1b06e6c8f6815fd68043009b9c
gms1 <www.gms@gmx.at>
2017-08-26 06:08:41 +0200

    chore(package): upgrade dev dependencies (tslint,jasmine...)

--- build/gulp/helpers.js
+++ build/gulp/helpers.js
@@ -2,31 +2,58 @@
 
 const path = require('path');
 
+// -----------------------------------------------------------------------
 function globify(...paths) {
   return path.join(...paths, '**', '*');
 }
 
 
-function updatePackageDependenciesVersions(deps, pkgPrefix, pkgVersion) {
-  if (!deps) return;
-  Object.getOwnPropertyNames(deps)
-      .filter((name) => {return name.startsWith(`${pkgPrefix}/`)})
-      .forEach((name) => {deps[name] = pkgVersion});
-}
-
+// -----------------------------------------------------------------------
 function updatePackageVersions(data, name, version) {
   data.version = version;
 
   const pkgNames = name.split('/');
   if (pkgNames.length>1) {
     const pkgPrefix = pkgNames[0];
-    updatePackageDependenciesVersions(data.dependencies, pkgPrefix, version);
-    updatePackageDependenciesVersions(data.devDependencies, pkgPrefix, version);
-    updatePackageDependenciesVersions(data.peerDependencies, pkgPrefix, version);
+    updatePackageDependenciesVersions(data.dependencies, pkgPrefix, name, version);
+    updatePackageDependenciesVersions(data.devDependencies, pkgPrefix, name, version);
+    updatePackageDependenciesVersions(data.peerDependencies, pkgPrefix, name, version);
+  } else {
+    updatePackageDependenciesVersions(data.dependencies, undefined, name, version);
+    updatePackageDependenciesVersions(data.devDependencies, undefined, name, version);
+    updatePackageDependenciesVersions(data.peerDependencies, undefined, name, version);
   }
   return data;
 }
 
 
+function updatePackageDependenciesVersions(deps, pkgPrefix, pkgName, pkgVersion) {
+  if (!deps) return;
+  if (deps[pkgName]) {
+    deps[pkgName] = pkgVersion;
+  }
+  if (!pkgPrefix) return;
+  Object.getOwnPropertyNames(deps)
+    .filter((name) => {return name.startsWith(`${pkgPrefix}/`)})
+    .forEach((name) => {deps[name] = pkgVersion});
+}
+
+// -----------------------------------------------------------------------
+function moveDependencies(data, propFrom, propTo) {
+  if (data[propFrom]) {
+    if (!data[propTo]) {
+      data[propTo] = {};
+    }
+    Object.keys(data[propFrom]).forEach((key) => {
+      data[propTo][key] = data[propFrom][key];
+    });
+    delete data[propFrom];
+  }
+}
+
+
+// -----------------------------------------------------------------------
+
 module.exports.globify = globify;
 module.exports.updatePackageVersions = updatePackageVersions;
+module.exports.moveDependencies = moveDependencies;
